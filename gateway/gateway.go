9e783a6f1a1bdad5e8a1ae18df07e1cc17b5fb4f
SmallL-U <smalll-u@qq.com>
2025-06-13 18:16:56 +0800

    feat: implement gateway start and stop logic

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,10 +1,16 @@
 package gateway
 
 import (
+	"errors"
+	"fmt"
 	"minecraft-gateway/config"
+	"minecraft-gateway/logx"
 	"net"
+	"strings"
 	"sync"
 )
+
+var logger = logx.GetLogger()
 
 type Gateway struct {
 	Config      *config.Config
@@ -16,10 +22,38 @@ func NewGateway(cfg *config.Config) *Gateway {
 	return &Gateway{Config: cfg}
 }
 
+func (g *Gateway) handleConnection(clientConn net.Conn) {
+	// TODO: Implement connection handling logic
+}
+
 func (g *Gateway) Start() error {
-	return nil
+	logger.Info("Starting gateway...")
+	listener, err := net.Listen("tcp", g.Config.ListenAddr)
+	if err != nil {
+		return fmt.Errorf("failed to start gateway: %s", err)
+	}
+	g.listener = listener
+	logger.Infof("Gateway listening on %s", g.Config.ListenAddr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			var opErr *net.OpError
+			if errors.As(err, &opErr) && opErr.Op == "accept" {
+				if strings.Contains(err.Error(), "use of closed network connection") {
+					logger.Info("Listener closed, shutting down gracefully")
+					return nil
+				}
+			}
+			logger.Errorf("Failed to accept connection: %s", err)
+			continue
+		}
+		go g.handleConnection(conn)
+	}
 }
 
 func (g *Gateway) Stop() error {
+	if g.listener != nil {
+		return g.listener.Close()
+	}
 	return nil
 }
