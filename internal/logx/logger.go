d524b1539870ee55fe11828e1e3d5d6e1f6c317c
SmallL-U <smalll-u@qq.com>
2026-01-24 19:39:01 +0800

    refactor(logx): use human-friendly console output format

--- internal/logx/logger.go
+++ internal/logx/logger.go
@@ -1,11 +1,41 @@
 package logx
 
-import "go.uber.org/zap"
+import (
+	"os"
 
-var logger = func() *zap.SugaredLogger {
-	production, _ := zap.NewProduction()
-	return production.Sugar()
-}()
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.SugaredLogger
+
+func init() {
+	logger = newConsoleLogger(zapcore.InfoLevel)
+}
+
+func newConsoleLogger(level zapcore.Level) *zap.SugaredLogger {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.AddSync(os.Stdout),
+		level,
+	)
+
+	return zap.New(core).Sugar()
+}
 
 func GetLogger() *zap.SugaredLogger {
 	return logger
